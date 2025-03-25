f00e0599788eec5df61b1ab1c28c27a70e872721
TwinkleX7777 <danishkhan94251@gmail.com>
2025-03-25 16:58:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBP1Cx8cmvjf24oY1gNiD_-qxis6v6pwNI",
+  authDomain: "taprace-63f8e.firebaseapp.com",
+  databaseURL: "https://taprace-63f8e-default-rtdb.firebaseio.com",
+  projectId: "taprace-63f8e",
+  storageBucket: "taprace-63f8e.firebasestorage.app",
+  messagingSenderId: "93332718324",
+  appId: "1:93332718324:web:4b48350c0b64061eb794a1"
+};
 let score = 0;
 let timeLeft = 10;
 let timer;
